4f64aa11211d0415b74516dddf29d0903c6b1e42
Antonio Vazquez <54569464+tonyekssj@users.noreply.github.com>
2022-09-08 11:53:48 -0700

    Practica_2

---
+++ Practica_2/prac2_receptor/sample_project/main/main.c
@@ -0,0 +1,372 @@
+/*Receptor*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include "myUart.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/gpio.h"
+#include "crc.h"
+
+
+#define UART_RX_PIN     (3)
+#define UART_TX_PIN     (1)
+
+#define UART1_RX_PIN     (9)
+#define UART1_TX_PIN     (10)
+
+#define UART2_RX_PIN     (16)
+#define UART2_TX_PIN     (17)
+
+#define UARTS_BAUD_RATE         (115200)
+#define TASK_STACK_SIZE         (1048)
+#define READ_BUF_SIZE           (1024)
+#define INC (temperatura++);
+
+static uint8_t estadoLed = 0;
+static uint8_t temperatura=10;
+
+struct uartSendPacket{
+
+    uint8_t init;
+    uint8_t command;
+    uint8_t longitudDato;
+    uint32_t dato;
+    uint8_t end;
+    uint32_t checkCRC32;
+
+}packageUart;
+
+void uartInit(uart_port_t uart_num, uint32_t baudrate, uint8_t size, uint8_t parity, uint8_t stop, uint8_t txPin, uint8_t rxPin)
+{
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = (int) baudrate,
+        .data_bits = (uart_word_length_t)(size-5),
+        .parity    = (uart_parity_t)parity,
+        .stop_bits = (uart_stop_bits_t)stop,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, READ_BUF_SIZE, READ_BUF_SIZE, 0, NULL, 0));
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(uart_num, txPin, rxPin,UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+}
+
+void uartGoto11(uart_port_t uart_num)
+{
+    // Limpie un poco el arreglo de caracteres, los siguientes tres son equivalentes:
+     // "\e[1;1H" == "\x1B[1;1H" == {27,'[','1',';','1','H'}
+    const char caGoto11[] = "\e[1;1H";
+    uart_write_bytes(uart_num, caGoto11, sizeof(caGoto11));
+}
+
+void delayMs(uint16_t ms)
+{
+    vTaskDelay(ms / portTICK_PERIOD_MS);
+}
+
+void uartClrScr(uart_port_t uart_num)
+{
+    // Uso "const" para sugerir que el contenido del arreglo lo coloque en Flash y no en RAM
+    const char caClearScr[] = "\e[2J";
+    uart_write_bytes(uart_num, caClearScr, sizeof(caClearScr));
+}
+
+bool uartKbhit(uart_port_t uart_num)
+{
+    uint8_t length;
+    uart_get_buffered_data_len(uart_num, (size_t*)&length);
+    return (length > 0);
+}
+
+void uartPutchar(uart_port_t uart_num, char c)
+{  
+    uart_write_bytes(uart_num, &c, sizeof(c));
+}
+
+char uartGetchar(uart_port_t uart_num)
+{ 
+    char c;
+    // Wait for a received byte
+    while(!uartKbhit(uart_num))
+    {
+        delayMs(10);
+    }
+    // read byte, no wait
+    uart_read_bytes(uart_num, &c, sizeof(c), 0);
+
+    return c;
+}
+
+void uartGets(uart_port_t com,char* str){
+    
+    char c=0;
+    uint8_t contador=0;
+
+    while(c!=13){
+
+        c=uartGetchar(com);
+
+        if((c==8) && (contador>0)){
+            contador--;
+            uartPutchar(com,8);
+            uartPutchar(com,' ');
+            uartPutchar(com,8);
+            *str-- =0;
+        }
+
+        else if (c!=8)
+        {
+            contador++;
+            uartPutchar(com,c); 
+            *str++=c;   
+        }
+                
+    }
+}
+
+void uartPuts(uart_port_t com, char *str){
+    while(*str){
+        uartPutchar(com,*str++);
+    }
+}
+
+void uartGotoxy(uart_port_t com, uint8_t x, uint8_t y){
+    char caGotoxy[] = "\e[0;0H";/*y;x*/
+    char caGotoxy2[] = "\e[00;00H";/*y;x*/
+    char c_y[] = "00";
+    char c_x[] ="00";
+    char c;
+
+    myItoa(x,c_x,10);
+    myItoa(y,c_y,10);
+    
+    if (x<=9 && y<=9){
+        c = y + '0';
+        caGotoxy[2]=c;
+        c = x + '0';
+        caGotoxy[4]=c;
+        uart_write_bytes(com, caGotoxy, sizeof(caGotoxy));
+    }
+    else{
+        caGotoxy2[2]=c_y[0];
+        caGotoxy2[3]=c_y[1];
+
+        caGotoxy2[5]=c_x[0];
+        caGotoxy2[6]=c_x[1];
+        uart_write_bytes(com, caGotoxy2, sizeof(caGotoxy2));      
+    }
+}
+
+uint16_t myAtoi(char *str){
+    uint16_t temp=0;
+    uint16_t num=0;
+    char c;
+
+    while(*str){
+
+		c=*str++;
+        temp=c-48;
+
+        if(c== '.')
+            break;
+            
+        if (c>='0' && c<='9'){
+            if(c==0){
+                num=num+temp;
+    	    }  
+
+            else{
+            num=num*10;
+            num=num+temp;
+    	    } 
+        }
+    }
+    return num;  
+}
+
+void myItoa(uint16_t number, char* str, uint8_t base){
+    char num_temp[15]={0};
+	uint16_t count = 0;
+	
+	if(number < 1 || base < 2) {
+		*str++ = '0';
+		*str = 0;
+		return;
+	}
+
+	while(number) {
+		if((number % base) > 9) num_temp[count++] = (number % base) + '0' + 7;
+		else num_temp[count++] = (number % base) + '0';
+		number -= (number % base);
+		number /= base;
+	}
+
+	while(count > 0) {
+		*str++ = num_temp[--count];
+	}
+
+	*str = 0;
+}
+
+
+void comandoTimestamp(uint32_t start){
+    uint32_t stop=0;
+    const char cad[]={"10"};
+
+    stop=xTaskGetTickCount()-start; 
+
+    sendPacket(1,cad,sizeof(stop),stop);
+
+}
+
+void comandoEstado(){
+    const char cad[]={"11"};
+    sendPacket(1,cad,sizeof(estadoLed),estadoLed);
+}
+
+void mostrarTemperatura(){
+    const char cad[]={"12"};
+    sendPacket(1,cad,sizeof(temperatura),temperatura);
+}
+
+void invertirEstado(){
+    const char cad[]={"13"};
+
+    estadoLed= !estadoLed;
+    gpio_set_level(2,estadoLed);
+    uartPuts(0,"Invertido");
+
+    sendPacket(1,cad,0,0);
+
+}
+
+
+
+bool checkPackage(){
+
+    uint8_t data[]={0x5a,packageUart.command,packageUart.longitudDato,packageUart.dato,0xb2}; 
+    uint32_t calculateCRC32;
+
+    calculateCRC32=crc32_le(0,data,sizeof(data));
+
+    if(calculateCRC32 == packageUart.checkCRC32)
+        return true;
+    else 
+        return false;
+}
+
+void receivePacket(uart_port_t uart_num){
+
+    packageUart.init=uartGetchar(uart_num);
+    packageUart.command=uartGetchar(uart_num);
+    packageUart.longitudDato=uartGetchar(uart_num);
+    packageUart.dato=uartGetchar(uart_num);
+    packageUart.end=uartGetchar(uart_num);
+
+    packageUart.checkCRC32=uartGetchar(uart_num);
+    packageUart.checkCRC32|=uartGetchar(uart_num) << 8;
+    packageUart.checkCRC32|=uartGetchar(uart_num) << 16;
+    packageUart.checkCRC32|=uartGetchar(uart_num) << 24;
+}
+
+void sendWrongpacket(uart_port_t uart_num){
+
+    packageUart.init=0x5a;
+    packageUart.command=0;
+    packageUart.longitudDato=0;
+    packageUart.dato=0;
+    packageUart.end=0xb2;
+
+    uartPutchar(uart_num,packageUart.init);
+    uartPutchar(uart_num,packageUart.command);
+    uartPutchar(uart_num,packageUart.longitudDato);
+    uartPutchar(uart_num,packageUart.dato);
+    uartPutchar(uart_num,packageUart.end);
+    //Send CRC32
+    uartPutchar(uart_num,packageUart.checkCRC32);
+    uartPutchar(uart_num,packageUart.checkCRC32>>8);
+    uartPutchar(uart_num,packageUart.checkCRC32>>16);
+    uartPutchar(uart_num,packageUart.checkCRC32>>24);
+
+}
+
+void sendPacket(uart_port_t uart_num, char *str,uint8_t longitudDato,uint32_t dato){
+
+    packageUart.init=0x5a;
+    packageUart.command=myAtoi(str);
+    packageUart.longitudDato=longitudDato;
+    packageUart.dato=dato;
+    packageUart.end=0xb2;
+
+    uint8_t dataSend[]={packageUart.init,packageUart.command,packageUart.longitudDato,packageUart.dato,packageUart.end};  
+    packageUart.checkCRC32=crc32_le(0,dataSend,sizeof(dataSend));
+
+    uartPutchar(uart_num,packageUart.init);
+    uartPutchar(uart_num,packageUart.command);
+    uartPutchar(uart_num,packageUart.longitudDato);
+    uartPutchar(uart_num,packageUart.dato);
+    uartPutchar(uart_num,packageUart.end);
+    //Send CRC32
+    uartPutchar(uart_num,packageUart.checkCRC32);
+    uartPutchar(uart_num,packageUart.checkCRC32>>8);
+    uartPutchar(uart_num,packageUart.checkCRC32>>16);
+    uartPutchar(uart_num,packageUart.checkCRC32>>24);
+
+}
+
+void app_main(void)
+{
+    uint32_t startTimestamp=xTaskGetTickCount();
+    gpio_reset_pin(2);
+    gpio_set_direction(2,GPIO_MODE_OUTPUT); //Configuro el LED del board para el comando 13 de invertir LED
+    
+    uartInit(0,UARTS_BAUD_RATE,8,0,1,UART_TX_PIN,UART_RX_PIN);
+    uartInit(1,UARTS_BAUD_RATE,8,0,1,UART2_TX_PIN,UART2_RX_PIN);
+
+    delayMs(1000);
+
+    while(1) 
+    {
+        uartClrScr(0);
+        uartPuts(0,"Waiting...");
+        receivePacket(1);
+
+        if (checkPackage()){
+            switch (packageUart.command)
+            {
+            case 10:
+                comandoTimestamp(startTimestamp);
+                uartPuts(0,"Enviado TimeStamp");
+                break;
+            case 11:
+                comandoEstado();
+                uartPuts(0,"Enviado estado de led");
+                break;
+            case 12:
+                INC;
+                mostrarTemperatura();
+                uartPuts(0,"Enviado temperatura");
+                break;
+            case 13:
+                invertirEstado();
+                uartPuts(0,"Invertir estado de led");
+                break;
+            default:
+                sendWrongpacket(1);
+                uartPuts(0,"*Comando Invalido*");
+                break;
+            }
+        }
+            
+        else 
+            sendWrongpacket(1);
+        delayMs(1000);
+        
+    }
+
+}
---
+++ Practica_2/prac2_transmisor/sample_project/main/myUart.h
@@ -0,0 +1,22 @@
+#include "driver/uart.h"
+
+void uartInit(uart_port_t uart_num, uint32_t baudrate, uint8_t size, uint8_t parity, uint8_t stop, uint8_t txPin, uint8_t rxPin);
+void uartGoto11(uart_port_t uart_num);
+
+// Send
+void uartPuts(uart_port_t uart_num, char *str);
+void uartPutchar(uart_port_t uart_num, char data);
+
+bool uartKbhit(uart_port_t uart_num);
+char uartGetchar(uart_port_t uart_num );
+void uartGets(uart_port_t uart_num, char *str);
+
+void uartClrScr( uart_port_t uart_num );
+
+void myItoa(uint32_t number, char* str, uint8_t base) ;
+uint16_t myAtoi(char *str);
+
+void sendPacket(uart_port_t uart_num, char *str,uint8_t longitudDato,uint32_t dato);
+void receivePacket(uart_port_t uart_num);
+
+bool checkPackage();
